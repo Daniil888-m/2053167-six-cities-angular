b46068358ce9cd04da051dfa6d6073908d689916
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-07-23 22:19:00 +0300

    созданы файлы для компонентов-страниц

---
+++ src/app/domains/favorites/pages/favorites/favorites.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites',
+  imports: [],
+  templateUrl: './favorites.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FavoritesComponent {
+
+}
---
+++ src/app/domains/offer/pages/offer/offer.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer',
+  imports: [],
+  templateUrl: './offer.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OfferComponent {
+
+}
