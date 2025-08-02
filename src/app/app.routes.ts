33321e10d3c31a2a8c0ac093ed45fdd163605aa0
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-08-02 16:47:23 +0300

    Добавлены роуты для всех страниц

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { OfferComponent } from './pages/offer/offer.component';
+import { NotFoundPageComponent } from './pages/not-found/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+  },
+  {
+    path: 'offer/:id',
+    component: OfferComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
