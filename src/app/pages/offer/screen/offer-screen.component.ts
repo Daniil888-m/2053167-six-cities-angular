3bb9b7895b6dacceb0246064ccdb748fd229071b
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-08-17 20:46:21 +0300

    С помощью компонента «Список отзывов», созданного в первом шаге, отрисован весь список отзывов

--- src/app/pages/offer/screen/offer-screen.component.ts
+++ src/app/pages/offer/screen/offer-screen.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ReviewFormComponent } from '../review-form/review-form.component';
+import { ReviewFormComponent } from '../components/review-form/review-form.component';
+import { reviewsMock } from '../../../mocks/reviews';
+import { ReviewsListComponent } from '../components/reviews-list/reviews-list.component';
 
 @Component({
   selector: 'app-offer-screen',
-  imports: [ReviewFormComponent],
+  imports: [ReviewFormComponent, ReviewsListComponent],
   templateUrl: './offer-screen.component.html',
   styleUrl: './offer-screen.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OfferScreenComponent {}
+export class OfferScreenComponent {
+  reviews = reviewsMock;
+}
