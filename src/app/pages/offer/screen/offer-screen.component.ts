ed8eca77acf406750b05f769e267f74a8672041f
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-08-20 19:59:16 +0300

    исправлен баг с картой, которая имела нулевую ширину.

--- src/app/pages/offer/screen/offer-screen.component.ts
+++ src/app/pages/offer/screen/offer-screen.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ReviewFormComponent } from '../components/review-form/review-form.component';
-import { reviewsMock } from '../../../mocks/reviews';
 import { ReviewsListComponent } from '../components/reviews-list/reviews-list.component';
 import { Offer, offersMock } from '../../../mocks/offers';
 import { MapComponent } from '../../../common/components/map/map.component';
 import { OfferFull, offerMock } from '../../../mocks/offer';
 import { NearbyListComponent } from '../components/nearby-list/nearby-list.component';
 import { ActiveCardService } from '../../main/services/active-card.service';
+import { reviewsMock } from '../../../mocks/reviews';
 
 @Component({
   selector: 'app-offer-screen',
