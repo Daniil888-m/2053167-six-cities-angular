9393034792d676fc7f4ee6128bd670f14d3cad0b
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-08-21 16:16:58 +0300

    Подключена и настроена форма на странице login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,6 +20,21 @@ export class LoginComponent {
   public loginForm: FormGroup;
 
   constructor() {
-    this.loginForm = this.fb.group({});
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
+        ],
+      ],
+    });
+  }
+
+  public onSubmit(): void {
+    const formData = this.loginForm.value;
+
+    console.log(formData);
   }
 }
