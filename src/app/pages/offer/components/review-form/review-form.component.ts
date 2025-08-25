8a2f389f2f4f5c6177da479f2ce87481accbde36
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-08-25 17:56:02 +0300

    Добавлена форма отправки отзыва

--- src/app/pages/offer/components/review-form/review-form.component.ts
+++ src/app/pages/offer/components/review-form/review-form.component.ts
@@ -1,14 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-review-form',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './review-form.component.html',
   styleUrl: './review-form.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReviewFormComponent {
-  public reviewText = '';
-  public reviewRate = 0;
+  private fb = inject(FormBuilder);
+  public reviewForm: FormGroup;
+
+  public ratingData = [
+    { value: 5, title: 'perfect' },
+    { value: 4, title: 'good' },
+    { value: 3, title: 'not bad' },
+    { value: 2, title: 'bad' },
+    { value: 1, title: 'terribly' },
+  ];
+
+  constructor() {
+    this.reviewForm = this.fb.group({
+      text: ['', [Validators.required, Validators.minLength(50)]],
+      rating: [0, [Validators.required, Validators.min(1)]],
+    });
+  }
+
+  public onSubmit() {
+    const data = this.reviewForm.value;
+    console.log(data);
+    this.reviewForm.reset();
+  }
 }
